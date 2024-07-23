c64310b504e50717d9ba60f33e6ca32421d15d00
Nuthan B <NuthanB@protonmail.com>
2024-07-23 18:55:34 +0530

    Updates

---
+++ precache-manifest.5608930ff952dd152a0c5c36fb960db4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01dfbbd75af4438d50ea542026d8785",
+    "url": "/NasaSpaceApp/index.html"
+  },
+  {
+    "revision": "1704811f742f71f44c27",
+    "url": "/NasaSpaceApp/static/css/main.ce07c409.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/NasaSpaceApp/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/NasaSpaceApp/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "1704811f742f71f44c27",
+    "url": "/NasaSpaceApp/static/js/main.493f88c9.chunk.js"
+  },
+  {
+    "revision": "f2aab5256f16b3ca3ea8",
+    "url": "/NasaSpaceApp/static/js/runtime~main.5aeef95f.js"
+  },
+  {
+    "revision": "1f7c43ba5189d9045481c59bc09cd5fa",
+    "url": "/NasaSpaceApp/static/media/bg.1f7c43ba.jpg"
+  },
+  {
+    "revision": "abfb848f925f6e1420234460f558fa3b",
+    "url": "/NasaSpaceApp/static/media/contact-bg.abfb848f.jpg"
+  }
+]);
